124fd874086183c0097d6004fc9ed7bc7de3a33f
yannawut <yannawut.s@gofive.co.th>
2022-06-16 19:58:28 +0700

    client side init

---
+++ socket-client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container fluid>
+      <LoginModal msg="Welcome to Your Vue.js App" />
+  </b-container>
+</template>
+
+<script>
+import LoginModal from "./components/Login.vue";
+
+export default {
+  name: "App",
+  components: {
+    LoginModal,
+  },
+};
+</script>
+
+<style>
+ b-container {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-image: url("./assets/background.jpg");
+}
+</style>
